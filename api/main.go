4f0724cc901b9d6f71349a8b300ca298c4fe3792
Proxyfil <piloupilou200@gmail.com>
2025-07-13 18:09:48 +0200

    fix(api): Moved methods to have better arch

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	db "api/db"
 	persons "api/struct"
 	apisecurity "api/utils/api_security"
+	routes "api/utils/routes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -17,218 +18,6 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/health" {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Healthy")
-}
-
-func getPersons(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/public/v1/persons" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Get MongoDB client from context
-	mongoClient := r.Context().Value("mongoClient").(*mongo.Client)
-
-	// Get persons from the database
-	personsList, err := db.GetPersons(mongoClient, "dodle")
-	if err != nil {
-		http.Error(w, "Failed to get persons: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Set response headers
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	// Encode persons as JSON and send response
-	if err := json.NewEncoder(w).Encode(personsList); err != nil {
-		http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func GetPersonsOfTheDay(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/private/v1/guess/persons" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Check if the request is authorized with API token
-	if !apisecurity.IsAuthorized(r) {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-
-	// Get MongoDB client from context
-	mongoClient := r.Context().Value("mongoClient").(*mongo.Client)
-
-	// Update person of the day
-	personsGuess, err := db.GetPersonsOfTheDay(mongoClient, "dodle")
-	if err != nil {
-		http.Error(w, "Failed to update person of the day: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Set response headers
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	if err := json.NewEncoder(w).Encode(personsGuess); err != nil {
-		http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func CreatePersonOfTheDay(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/private/v1/guess/person/create" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Check if the request is authorized with API token
-	if !apisecurity.IsAuthorized(r) {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-
-	// Get MongoDB client from context
-	mongoClient := r.Context().Value("mongoClient").(*mongo.Client)
-
-	// Update person of the day
-	if err := UpdatePersonOfTheDay(mongoClient); err != nil {
-		http.Error(w, "Failed to update person of the day: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Person of the day updated successfully")
-}
-
-func UpdatePersonOfTheDay(mongoClient *mongo.Client) error {
-	previousPersons, err := db.GetPersonsOfTheDay(mongoClient, "dodle")
-	if err != nil {
-		return fmt.Errorf("failed to get previous persons of the day: %v", err)
-	}
-
-	personsAvailable, err := db.GetPersons(mongoClient, "dodle")
-	if err != nil {
-		return fmt.Errorf("failed to get persons: %v", err)
-	}
-
-	if len(personsAvailable.Persons) == 0 {
-		return fmt.Errorf("no persons available to update person of the day")
-	}
-
-	isSelectable := false
-	candidate := personsAvailable.Persons[rand.Intn(len(personsAvailable.Persons))]
-	fmt.Println("New candidate for person of the day:", candidate.Firstname, candidate.Lastname)
-	for i := 0; i < len(personsAvailable.Persons); i++ {
-		candidate = personsAvailable.Persons[rand.Intn(len(personsAvailable.Persons))]
-		isSelectable = true
-		for _, person := range previousPersons {
-			if person.Firstname == candidate.Firstname && person.Lastname == candidate.Lastname {
-				isSelectable = false
-			}
-		}
-
-		if isSelectable {
-			break
-		}
-	}
-
-	dateOfToday := time.Now().Format("2006-01-02")
-
-	// If we already selected a candidate today we delete the previous one
-	if GetPersonOfTheDay, err := db.GetPersonOfTheDay(mongoClient, "dodle"); err == nil {
-		if GetPersonOfTheDay.Firstname != "" {
-			if err := db.DeletePersonOfTheDay(mongoClient, "dodle", dateOfToday); err != nil {
-				return fmt.Errorf("failed to delete previous person of the day: %v", err)
-			}
-			fmt.Println("Previous person of the day deleted successfully")
-		}
-	}
-
-	if err := db.CreatePersonOfTheDay(mongoClient, "dodle", candidate); err != nil {
-		return fmt.Errorf("failed to create person of the day: %v", err)
-	}
-
-	dateToDelete := time.Now().AddDate(0, 0, -10).Format("2006-01-02")
-
-	if err := db.DeletePersonOfTheDay(mongoClient, "dodle", dateToDelete); err != nil {
-		return fmt.Errorf("failed to delete previous person of the day: %v", err)
-	}
-	fmt.Println("Previous person of the day deleted successfully")
-	// You can add your logic here to update the person of the day in the database.
-	return nil
-}
-
-func GuessPersonOfTheDay(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/public/v1/guess/person/submit" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Get MongoDB client from context
-	mongoClient := r.Context().Value("mongoClient").(*mongo.Client)
-
-	// Decode the guess from the request body
-	var guess persons.Person
-	if err := json.NewDecoder(r.Body).Decode(&guess); err != nil {
-		http.Error(w, "Invalid request body: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	fmt.Println("Received guess:", guess)
-
-	// Try to guess the person of the day
-	correct, returnedPerson, err := db.TryGuess(mongoClient, "dodle", guess)
-	if err != nil {
-		http.Error(w, "Failed to process guess: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	if err := json.NewEncoder(w).Encode(map[string]interface{}{
-		"correct": correct,
-		"person":  returnedPerson,
-	}); err != nil {
-		http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func GetHint(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/public/v1/guess/person/hint" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Get MongoDB client from context
-	mongoClient := r.Context().Value("mongoClient").(*mongo.Client)
-
-	// Get person of the day
-	personOfTheDay, err := db.GetPersonOfTheDay(mongoClient, "dodle")
-	if err != nil {
-		http.Error(w, "Failed to get person of the day: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Set response headers
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	if err := json.NewEncoder(w).Encode(personOfTheDay.Hint); err != nil {
-		http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
 
 // Middleware to inject MongoDB client into request context
 func withMongoClient(next http.Handler, client *mongo.Client) http.Handler {
@@ -274,15 +63,15 @@ func main() {
 	// Create router
 	mux := http.NewServeMux()
 
-	// Register handlers
-	mux.HandleFunc("/health", healthHandler)
-
 	// Wrap handlers with MongoDB client middleware
-	personsHandler := http.HandlerFunc(getPersons)
-	guessHandler := http.HandlerFunc(GetPersonsOfTheDay)
-	createPODHandler := http.HandlerFunc(CreatePersonOfTheDay)
-	guessPersonHandler := http.HandlerFunc(GuessPersonOfTheDay)
-	getHintHandler := http.HandlerFunc(GetHint)
+	personsHandler := http.HandlerFunc(routes.GetPersons)
+	guessHandler := http.HandlerFunc(routes.GetPersonsOfTheDay)
+	createPODHandler := http.HandlerFunc(routes.CreatePersonOfTheDay)
+	guessPersonHandler := http.HandlerFunc(routes.GuessPersonOfTheDay)
+	getHintHandler := http.HandlerFunc(routes.GetHint)
+
+	// Register handlers
+	mux.HandleFunc("/health", routes.HealthHandler)
 	mux.Handle("/public/v1/persons", withMongoClient(personsHandler, mongoClient))
 	mux.Handle("/private/v1/guess/persons", withMongoClient(guessHandler, mongoClient))
 	mux.Handle("/private/v1/guess/person/create", withMongoClient(createPODHandler, mongoClient))
