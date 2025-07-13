9155a36242d55fee762ea1216638fe61539f634b
Proxyfil <piloupilou200@gmail.com>
2025-07-13 17:57:40 +0200

    fix(api): Moved some methods to match projet organisation

---
+++ api/utils/data.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func OpenPersonsFile() (persons.Persons, error) {
+	jsonFile, err := os.Open("./data/persons.json")
+	if err != nil {
+		return persons.Persons{}, fmt.Errorf("error opening file: %v", err)
+	}
+	defer jsonFile.Close()
+
+	// Read the content of the file
+	content, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return persons.Persons{}, fmt.Errorf("error reading file: %v", err)
+	}
+
+	// Unmarshal the JSON data into the struct
+	var personsList []persons.Person
+	err = json.Unmarshal(content, &personsList)
+	if err != nil {
+		return persons.Persons{}, fmt.Errorf("error unmarshaling JSON: %v", err)
+	}
+
+	return persons.Persons{Persons: personsList}, nil
+}
